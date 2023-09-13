512fc3545bef4f58eed87aa94e7efd54ba799cbb
Anton Ablov <a.d.ablov@gmail.com>
2023-09-13 12:17:52 +0200

    Put latex macros to common .js. Update mathjax

---
+++ AlgebraicTopology/scripts/texdefs.js
@@ -0,0 +1,22 @@
+window.MathJax = {
+    tex: {
+        macros: {
+            id: "{\\operatorname{id}}",
+            Fr: "{\\text{Fr}}",
+            ker: "{\\operatorname{ker}}",
+            im: "{\\operatorname{im}}",
+            Forget: "{\\operatorname{Forget}}",
+            Free: "{\\operatorname{Free}}",
+            mZ: "{\\mathbb{Z}}",
+            mN: "{\\mathbb{N}}",
+            mQ: "{\\mathbb{Q}}",
+            mR: "{\\mathbb{R}}",
+            inv: "{^{-1}}",
+            normal: "{\\,\\triangleleft\\,}",
+            la: "{\\langle}",
+            ra: "{\\rangle}",
+            mcN: "{\\mathcal{N}}",
+            mcI: "{\\mathcal{I}}"
+        }
+    }
+};
